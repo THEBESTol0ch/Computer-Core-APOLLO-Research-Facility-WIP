4c64312ab95ffa8a52fe092b0c55bda1f13680ec
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-02-26 18:07:00 +1000

    Create SecurityJoinScript.lua

---
+++ HUD/MainMenu/Menu/TeamsTab/SecurityJoinScript.lua
@@ -0,0 +1,25 @@
+local Team = game.Teams.Security
+local Player = game:GetService("Players").LocalPlayer
+local Group = game:GetService("GroupService")
+local Groupid = 11755642
+
+local PlayerRank = Player:GetRankInGroup(Groupid)
+local MemberRank = 1
+
+local RedNot3 = script.Parent.Parent.Parent.Parent.Parent.Parent.Notifications.RedNot3
+local NotificationSound = script.Parent.Parent.Parent.Parent.Parent.Parent.NotificationSound
+
+script.Parent.MouseButton1Click:Connect(function()
+	if PlayerRank >= MemberRank then
+		game.ReplicatedStorage.ChangeTeam:FireServer(Team)
+		game.Players.LocalPlayer.Character.Humanoid.Health = 0
+	else
+		NotificationSound:Play()
+		RedNot3.Visible = 0
+		RedNot3:TweenPosition(UDim2.new(0.908, 0, 0.93, 0), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 0.5, false)
+		wait(2)
+		RedNot3:TweenPosition(UDim2.new(1.09, 0, 0.93, 0), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 0.5, false)
+		wait(1)
+		RedNot3.Visible = 1
+	end
+end)
