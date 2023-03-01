6862ceb48bacc801032a10040a177222e4dd3b9e
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-01 16:22:50 +1000

    Create KeyboardScript.lua

---
+++ TNER/TNERCodeComputer/KeyboardScript.lua
@@ -0,0 +1,73 @@
+-- RSU 09R74S
+-- RSD 82S65D
+
+-- Control
+local TweenService = game:GetService("TweenService")
+local KeyboardTrigger = script.Parent.Parent.Parent.KeyboardTrigger
+--
+
+-- Values
+local GeneratorStatusValue = workspace.TNERGenerator.CPU.Values.GeneratorStatusValue
+local TNERStatusValue = workspace.TNERCodeComputer.TNERCodeMonitor.CPU.Scripts.TNERStatusValue
+--
+
+-- Items
+local KeysAnimationSettings = TweenInfo.new(
+	0.05,
+	Enum.EasingStyle.Quint,
+	Enum.EasingDirection.Out,
+	0,
+	false,
+	0
+)
+local CodeSequences = {
+	["OFFLINE"] = {
+		{ WaitTime = 0.585, Key = script.Parent.Parent.Parent.Keys.Key0.Key0, Pos = script.Parent.Parent.Parent.Keys.Key0.KeyPos0 },
+		{ WaitTime = 0.230, Key = script.Parent.Parent.Parent.Keys.Key9.Key9, Pos = script.Parent.Parent.Parent.Keys.Key9.KeyPos9 },
+		{ WaitTime = 0.277, Key = script.Parent.Parent.Parent.Keys.KeyR.KeyR, Pos = script.Parent.Parent.Parent.Keys.KeyR.KeyPosR },
+		{ WaitTime = 0.8, Key = script.Parent.Parent.Parent.Keys.Key7.Key7, Pos = script.Parent.Parent.Parent.Keys.Key7.KeyPos7 },
+		{ WaitTime = 0.2, Key = script.Parent.Parent.Parent.Keys.Key4.Key4, Pos = script.Parent.Parent.Parent.Keys.Key4.KeyPos4 },
+		{ WaitTime = 0.2, Key = script.Parent.Parent.Parent.Keys.KeyS.KeyS, Pos = script.Parent.Parent.Parent.Keys.KeyS.KeyPosS },
+		{ WaitTime = 0.3, Key = script.Parent.Parent.Parent.Keys.KeyEnter.KeyEnter, Pos = script.Parent.Parent.Parent.Keys.KeyEnter.KeyPosEnter },
+	},
+	["ONLINE"] = {
+		{ WaitTime = 0.585, Key = script.Parent.Parent.Parent.Keys.Key8.Key8, Pos = script.Parent.Parent.Parent.Keys.Key8.KeyPos8 },
+		{ WaitTime = 0.230, Key = script.Parent.Parent.Parent.Keys.Key2.Key2, Pos = script.Parent.Parent.Parent.Keys.Key2.KeyPos2 },
+		{ WaitTime = 0.277, Key = script.Parent.Parent.Parent.Keys.KeyS.KeyS, Pos = script.Parent.Parent.Parent.Keys.KeyS.KeyPosS },
+		{ WaitTime = 0.8, Key = script.Parent.Parent.Parent.Keys.Key6.Key6, Pos = script.Parent.Parent.Parent.Keys.Key6.KeyPos6 },
+		{ WaitTime = 0.2, Key = script.Parent.Parent.Parent.Keys.Key5.Key5, Pos = script.Parent.Parent.Parent.Keys.Key5.KeyPos5 },
+		{ WaitTime = 0.2, Key = script.Parent.Parent.Parent.Keys.KeyD.KeyD, Pos = script.Parent.Parent.Parent.Keys.KeyD.KeyPosD },
+		{ WaitTime = 0.3, Key = script.Parent.Parent.Parent.Keys.KeyEnter.KeyEnter, Pos = script.Parent.Parent.Parent.Keys.KeyEnter.KeyPosEnter },
+	}
+}
+--
+
+-- Sounds
+local KeyboardSound = script.Parent.Parent.Parent.SoundEmitter.KeyboardSound
+--
+
+-- Functions
+function ActivateCode(Sequence)
+	KeyboardSound:Play()
+	KeyboardTrigger.ClickDetector.MaxActivationDistance = 0
+	for _, Step in pairs(CodeSequences[Sequence]) do
+		wait(Step.WaitTime)
+		TweenService:Create(Step.Key, KeysAnimationSettings, { CFrame = Step.Pos.CFrame:ToWorldSpace(CFrame.new(0, 0, 0)) }):Play()
+		wait(0.05)
+		TweenService:Create(Step.Key, KeysAnimationSettings, { CFrame = Step.Key.CFrame:ToWorldSpace(CFrame.new(0, 0.035, 0)) }):Play()
+	end
+	wait(10) -- Code Accepted Animation
+	KeyboardTrigger.ClickDetector.MaxActivationDistance = 10
+end
+--
+
+KeyboardTrigger.ClickDetector.MouseClick:Connect(function()
+	ActivateCode(TNERStatusValue.Value)
+end)
+
+GeneratorStatusValue.Changed:Connect(function()
+	if GeneratorStatusValue.Value == "ONLINE" then
+		wait(3) -- Loading Screen Animation
+		KeyboardTrigger.ClickDetector.MaxActivationDistance = 10
+	end
+end)
