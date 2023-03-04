ba30e2df0b721a4b9ee16525f3b5f973c60d3fae
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-04 14:59:19 +1000

    Create AdvancedLeverScript.lua

---
+++ TNER/TNERDriver/AdvancedLeverScript.lua
@@ -0,0 +1,96 @@
+-- Control
+local TweenService = game:GetService("TweenService")
+local Trigger = script.Parent.Lever.Handle
+local Switch = script.Parent.Parent.Switch.Handle
+--
+
+-- Values
+local TNERDriverStatusValue = script.Parent.Parent.CPU.Values.TNERDriverStatusValue
+--
+
+-- Items
+local HandleHinge = script.Parent.Hinge
+local RedLamp = script.Parent.Parent.Rod.AdvancedLeverFake.RedLamp.Lamp
+local GreenLamp = script.Parent.Parent.Rod.AdvancedLeverFake.GreenLamp.Lamp
+local Handle = script.Parent.Handle
+local HandlePos = script.Parent.HandlePos
+--
+
+-- Sounds
+local LeverSound = script.Parent.SoundEmitter.LeverSound
+--
+
+-- Functions
+function DoLamp(Lamp, Color, Mode)
+	if Color == "Red" then
+		if Mode == "ON" then
+			Lamp.BrickColor = BrickColor.new("Really red")
+			Lamp.Material = ("Neon")
+		elseif Mode == "OFF" then
+			Lamp.BrickColor = BrickColor.new("Crimson")
+			Lamp.Material = ("Plastic")
+		end
+	elseif Color == "Green" then
+		if Mode == "ON" then
+			Lamp.BrickColor = BrickColor.new("Lime green")
+			Lamp.Material = ("Neon")
+		elseif Mode == "OFF" then
+			Lamp.BrickColor = BrickColor.new("Forest green")
+			Lamp.Material = ("Plastic")
+		end
+	end
+end
+--
+
+local LeverAnimationSettings = TweenInfo.new(
+	0.5,
+	Enum.EasingStyle.Sine,
+	Enum.EasingDirection.In,
+	0,
+	false,
+	0
+)
+
+local LeverDown = TweenService:Create(HandleHinge, LeverAnimationSettings, {
+	CFrame = HandleHinge.CFrame * CFrame.Angles(math.rad(-79.89), 0, 0)
+})
+local LeverUp = TweenService:Create(HandleHinge, LeverAnimationSettings, {
+	CFrame = HandleHinge.CFrame * CFrame.Angles(math.rad(-0), 0, 0)
+})
+
+local Handle2Down = {
+	CFrame = HandlePos.CFrame:ToWorldSpace(CFrame.new(0, 0, 0))
+}
+local Handle2Up = {
+	CFrame = Handle.CFrame:ToWorldSpace(CFrame.new(0, 0, 0))
+}
+
+local Handle2Down = TweenService:Create(Handle, LeverAnimationSettings, Handle2Down)
+local Handle2Up = TweenService:Create(Handle, LeverAnimationSettings, Handle2Up)
+
+Trigger.ClickDetector.MouseClick:Connect(function()
+	Trigger.ClickDetector.MaxActivationDistance = 0
+	LeverDown:Play()
+	Handle2Down:Play()
+	LeverSound:Play()
+	wait(1)
+	LeverUp:Play()
+	Handle2Up:Play()
+	wait(0.5)
+	Trigger.ClickDetector.MaxActivationDistance = 10
+end)
+
+TNERDriverStatusValue.Changed:Connect(function()
+	if TNERDriverStatusValue.Value == "MOVING" then
+		Trigger.ClickDetector.MaxActivationDistance = 0
+		DoLamp(GreenLamp, "Green", "OFF")
+		DoLamp(RedLamp, "Red", "ON")
+	end
+end)
+
+Switch.ClickDetector.MouseClick:Connect(function()
+	wait(1)
+	Trigger.ClickDetector.MaxActivationDistance = 10
+	DoLamp(GreenLamp, "Green", "ON")
+	DoLamp(RedLamp, "Red", "OFF")
+end)
