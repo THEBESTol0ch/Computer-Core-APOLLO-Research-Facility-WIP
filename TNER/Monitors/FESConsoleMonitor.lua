04fa9e1a360399ffeef393fae1c8ea14a38b58ee
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-06 19:42:18 +1000

    Update FESConsoleMonitor.lua

--- TNER/Monitors/FESConsoleMonitor.lua
+++ TNER/Monitors/FESConsoleMonitor.lua
@@ -5,6 +5,10 @@ local GeneratorStatusValue = workspace.TNERGenerator.CPU.Values.GeneratorStatusV
 -- Items
 local LoadingScreen = script.Parent.LoadingScreen.ApolloCorporationDecal
 local Lines = script.Parent.Lines
+
+local FEM1 = script.Parent.FEM1
+local FEM2 = script.Parent.FEM2
+local FEM3 = script.Parent.FEM3
 --
 
 GeneratorStatusValue.Changed:Connect(function()
@@ -16,5 +20,8 @@ GeneratorStatusValue.Changed:Connect(function()
 		for _, Object in pairs(Lines:GetChildren()) do
 			Object.SurfaceGui.TextLabel.Visible = true
 		end
+		FEM1.FEMOfflineDecal.Transparency = 0
+		FEM2.FEMOfflineDecal.Transparency = 0
+		FEM3.FEMOfflineDecal.Transparency = 0
 	end
 end)
