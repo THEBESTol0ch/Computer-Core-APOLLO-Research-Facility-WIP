e3214272b8a560c703e7410cd00d3f39084f3a04
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-10 23:59:44 +1000

    Create

---
+++ TNER/TNERCoolingSystem/CSEngageButtonScript.lua
@@ -0,0 +1,48 @@
+-- Control
+local TweenService = game:GetService("TweenService")
+local Button = script.Parent.Button
+--
+
+-- Values
+local ServerStatusValue = workspace.TNERSystemServer.Servers.CoolingSystemServer.CPU.Values.ServerStatusValue
+--
+
+-- Items
+local ButtonPos = script.Parent.ButtonPos
+--
+
+-- Sounds
+local ButtonSound = script.Parent.SoundEmitter.ButtonSound
+--
+
+local ButtonAnimationSettings = TweenInfo.new(
+	0.2,
+	Enum.EasingStyle.Quint,
+	Enum.EasingDirection.Out,
+	0,
+	false,
+	0
+)
+
+local ButtonDown = {
+	CFrame = ButtonPos.CFrame:ToWorldSpace(CFrame.new(0, 0, 0))
+}
+local ButtonUp = {
+	CFrame = Button.CFrame:ToWorldSpace(CFrame.new(0, 0, 0))
+}
+
+local ButtonDown = TweenService:Create(Button, ButtonAnimationSettings, ButtonDown)
+local ButtonUp = TweenService:Create(Button, ButtonAnimationSettings, ButtonUp)
+
+Button.ClickDetector.MouseClick:Connect(function()
+	ButtonDown:Play()
+	ButtonSound:Play()
+	wait(0.4)
+	ButtonUp:Play()
+end)
+
+ServerStatusValue.Changed:Connect(function()
+	if ServerStatusValue.Value == "ONLINE" then
+		Button.ClickDetector.MaxActivationDistance = 10
+	end
+end)
