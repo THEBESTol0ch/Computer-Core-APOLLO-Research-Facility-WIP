3fc5f5b8a7751dff59cb35b8b3f93f76bda92e24
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-11 16:45:33 +1000

    Create

---
+++ TNER/TNERPullLever/LeverScript.lua
@@ -0,0 +1,62 @@
+-- Control
+local TweenService = game:GetService("TweenService")
+local Trigger = script.Parent.Parent.Parent.Lever.Trigger
+--
+
+-- Values
+local LeverPositionValue = script.Parent.Parent.Values.LeverPositionValue
+local PreviousLeverPositionValue = script.Parent.Parent.Values.PreviousLeverPositionValue
+local TNERStatusValue = workspace.TNER.CPU.Values.TNERStatusValue
+--
+
+-- Items
+local Lever = script.Parent.Parent.Parent.Lever.Handle
+local Positions = script.Parent.Parent.Parent.Lever.Positions
+--
+
+-- Sounds
+local LeverSound = script.Parent.Parent.Parent.Lever.Handle.LeverSound
+local Alarm = workspace.TNERAlarmSystem.SoundEmitter.Alarm3
+--
+
+local PullLeverAnimationSettings = TweenInfo.new(
+	1,
+	Enum.EasingStyle.Sine,
+	Enum.EasingDirection.InOut,
+	0,
+	false,
+	0
+)
+
+Trigger.ClickDetector.MouseClick:Connect(function()
+	if LeverPositionValue.Value < 5 then
+		PreviousLeverPositionValue.Value = LeverPositionValue.Value
+		LeverPositionValue.Value = LeverPositionValue.Value + 1
+	end
+end)
+
+Trigger.ClickDetector.RightMouseClick:Connect(function()
+	if LeverPositionValue.Value > 1 then
+		PreviousLeverPositionValue.Value = LeverPositionValue.Value
+		LeverPositionValue.Value = LeverPositionValue.Value - 1
+	end
+end)
+
+LeverPositionValue.Changed:Connect(function()
+	Trigger.ClickDetector.MaxActivationDistance = 0
+	TweenService:Create(Lever, PullLeverAnimationSettings, { CFrame = Positions["LeverPos"..LeverPositionValue.Value].CFrame:ToWorldSpace(CFrame.new(0, 0, 0)) }):Play()
+	LeverSound:Play()
+	wait(1)
+	if TNERStatusValue.Value == "ONLINE" then
+		Trigger.ClickDetector.MaxActivationDistance = 10
+	end
+end)
+
+TNERStatusValue.Changed:Connect(function()
+	if TNERStatusValue.Value == "ONLINE" then
+		Trigger.ClickDetector.MaxActivationDistance = 10
+	elseif TNERStatusValue.Value == "SHUT DOWN" then
+		Trigger.ClickDetector.MaxActivationDistance = 0	
+		LeverPositionValue.Value = 3
+	end
+end)
