e9528cac8a9609933d1203d34611da18bc088f88
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-12 01:11:07 +1000

    Update FuelSystemScript.lua

--- TNER/TNERFuelSystem/FuelSystemScript.lua
+++ TNER/TNERFuelSystem/FuelSystemScript.lua
@@ -1,5 +1,14 @@
+-- Control
+local Trigger = workspace.TNERMaintenanceConsole.CapButton.Button
+--
+
 -- Values
 local FuelCapacityValue = script.Parent.Parent.Values.FuelCapacityValue
+local AllFuelCellsInjectedValue = script.Parent.Parent.Values.AllFuelCellsInjectedValue
+--
+
+-- Sounds
+local ActionAlarm = workspace.TNERAlarmSystem.SoundEmitter.Alarm3
 --
 
 -- Monitoring
@@ -10,6 +19,7 @@ FuelCapacityValue.Changed:Connect(function()
 	FuelCapacityLable.Text = FuelCapacityValue.Value
 	if FuelCapacityValue.Value == 99 then
 		FuelCapacityValue.Value = 100
+		AllFuelCellsInjectedValue.Value = true
 	elseif FuelCapacityValue.Value > 50 then
 		FuelCapacityLable.TextColor3 = Color3.new(0, 1, 0)
 	elseif FuelCapacityValue.Value < 50 then
@@ -17,4 +27,9 @@ FuelCapacityValue.Changed:Connect(function()
 	elseif FuelCapacityValue.Value < 25 then
 		FuelCapacityLable.TextColor3 = Color3.new(1, 0, 0)
 	end
+end)
+
+Trigger.ClickDetector.MouseClick:Connect(function()
+	ActionAlarm:Play()
+	AllFuelCellsInjectedValue.Value = false
 end)
