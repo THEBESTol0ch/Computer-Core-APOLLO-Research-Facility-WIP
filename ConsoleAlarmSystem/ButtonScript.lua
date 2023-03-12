418b6585e279f9495e10f9622ba50376fbb12579
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-12 17:55:24 +1000

    Create

---
+++ ConsoleAlarmSystem/ButtonScript.lua
@@ -0,0 +1,47 @@
+-- Transform Parameters
+-- 0.218 Studs
+--
+
+-- Control
+local TweenService = game:GetService("TweenService")
+local Button = script.Parent.Button
+--
+
+-- Items
+local ButtonPos = script.Parent.ButtonPos
+--
+
+-- Sounds
+local ButtonSound = script.Parent.Button.ButtonSound
+local SoundEmitter = script.Parent.Parent.ConsoleSpeaker.SoundEmitter
+--
+
+local ButtonAnimationSettings = TweenInfo.new(
+	0.2,
+	Enum.EasingStyle.Quint,
+	Enum.EasingDirection.Out,
+	0,
+	false,
+	0
+)
+
+local ButtonDown = {
+	CFrame = ButtonPos.CFrame:ToWorldSpace(CFrame.new(0, 0, 0))
+}
+local ButtonUp = {
+	CFrame = Button.CFrame:ToWorldSpace(CFrame.new(0, 0, 0))
+}
+
+local ButtonDown = TweenService:Create(Button, ButtonAnimationSettings, ButtonDown)
+local ButtonUp = TweenService:Create(Button, ButtonAnimationSettings, ButtonUp)
+
+Button.ClickDetector.MouseClick:Connect(function()
+	Button.ClickDetector.MaxActivationDistance = 0
+	ButtonDown:Play()
+	ButtonSound:Play()
+	wait(0.4)
+	ButtonUp:Play()
+	Button.Material = ("Metal")
+	SoundEmitter[script.Parent.Name]:Stop()
+	Button.ClickDetector.MaxActivationDistance = 10
+end)
