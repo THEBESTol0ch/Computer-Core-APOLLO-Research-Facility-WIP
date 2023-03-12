d909f62c1f2aad8f52a8a54fee5031ffcc644137
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-13 00:58:07 +1000

    Update FuelSystemScript.lua

--- TNER/TNERFuelSystem/FuelSystemScript.lua
+++ TNER/TNERFuelSystem/FuelSystemScript.lua
@@ -1,10 +1,11 @@
 -- Control
-local Trigger = workspace.TNERMaintenanceConsole.CapButton.Button
+local Trigger = workspace.TNERMaintenanceModeConsole.CapButton.Button
 --
 
 -- Values
 local FuelCapacityValue = script.Parent.Parent.Values.FuelCapacityValue
 local AllFuelCellsInjectedValue = script.Parent.Parent.Values.AllFuelCellsInjectedValue
+local TNERStatusValue = workspace.TNER.CPU.Values.TNERStatusValue
 --
 
 -- Sounds
@@ -17,19 +18,15 @@ local FuelCapacityLable = workspace.FuelSystemMonitor.Monitor.Lines.FuelCapacity
 
 FuelCapacityValue.Changed:Connect(function()
 	FuelCapacityLable.Text = FuelCapacityValue.Value
-	if FuelCapacityValue.Value == 99 then
+	if FuelCapacityValue.Value == 99 and TNERStatusValue.Value == "MAINTENANCE MODE" then
 		FuelCapacityValue.Value = 100
 		AllFuelCellsInjectedValue.Value = true
-	elseif FuelCapacityValue.Value > 50 then
-		FuelCapacityLable.TextColor3 = Color3.new(0, 1, 0)
-	elseif FuelCapacityValue.Value < 50 then
-		FuelCapacityLable.TextColor3 = Color3.new(1, 0.666667, 0)
-	elseif FuelCapacityValue.Value < 11 then
-		FuelCapacityLable.TextColor3 = Color3.new(1, 0, 0)
 	end
 end)
 
 Trigger.ClickDetector.MouseClick:Connect(function()
 	ActionAlarm:Play()
-	AllFuelCellsInjectedValue.Value = false
+	if FuelCapacityValue.Value < 11 then
+		AllFuelCellsInjectedValue.Value = false
+	end
 end)
