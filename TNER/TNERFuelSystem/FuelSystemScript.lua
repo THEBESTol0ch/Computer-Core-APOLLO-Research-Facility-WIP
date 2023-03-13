a4ca0ecf436481813ffb659088ae6f6f74fe0bd5
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-13 22:14:31 +1000

    Update FuelSystemScript.lua

--- TNER/TNERFuelSystem/FuelSystemScript.lua
+++ TNER/TNERFuelSystem/FuelSystemScript.lua
@@ -1,5 +1,5 @@
 -- Control
-local Trigger = workspace.TNERMaintenanceModeConsole.CapButton.Button
+local MaintenanceModeButton = workspace.TNERMaintenanceModeConsole.CapButton.Button
 --
 
 -- Values
@@ -12,19 +12,14 @@ local TNERStatusValue = workspace.TNER.CPU.Values.TNERStatusValue
 local ActionAlarm = workspace.TNERAlarmSystem.SoundEmitter.Alarm3
 --
 
--- Monitoring
-local FuelCapacityLable = workspace.FuelSystemMonitor.Monitor.Lines.FuelCapacity.SurfaceGui.TextLabel
---
-
 FuelCapacityValue.Changed:Connect(function()
-	FuelCapacityLable.Text = FuelCapacityValue.Value
 	if FuelCapacityValue.Value == 99 and TNERStatusValue.Value == "MAINTENANCE MODE" then
 		FuelCapacityValue.Value = 100
 		AllFuelCellsInjectedValue.Value = true
 	end
 end)
 
-Trigger.ClickDetector.MouseClick:Connect(function()
+MaintenanceModeButton.ClickDetector.MouseClick:Connect(function()
 	ActionAlarm:Play()
 	if FuelCapacityValue.Value < 11 then
 		AllFuelCellsInjectedValue.Value = false
