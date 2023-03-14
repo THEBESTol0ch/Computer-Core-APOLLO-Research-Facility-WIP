7170e2fc9c6aa2f4282da2b9787f11c4bbd64fd2
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-14 13:02:19 +1000

    Update TotalRPMScript.lua

--- TNER/SuperchargerSystem/TotalRPMScript.lua
+++ TNER/SuperchargerSystem/TotalRPMScript.lua
@@ -4,31 +4,34 @@ local SuperchargerTotalRPMValue = script.Parent.Parent.Values.SuperchargerTotalR
 --
 
 -- Items
-local SuperchargerTotalRPMLable = workspace.SuperchargerSystemMonitor.Monitor.Lines.TotalRPM.SurfaceGui.TextLabel
+local SuperchargerTotalRPMLabel = workspace.SuperchargerSystemMonitor.Monitor.Lines.TotalRPM.SurfaceGui.TextLabel
 --
 
 SuperchargerStatusValue.Changed:Connect(function()
-	if SuperchargerStatusValue.Value == "ONLINE" then
+	if SuperchargerStatusValue.Value == "INCREASE" then
 		repeat
 			SuperchargerTotalRPMValue.Value = SuperchargerTotalRPMValue.Value + 263
 			wait(0.07)
 		until
 		SuperchargerTotalRPMValue.Value > 23475 or SuperchargerStatusValue.Value == "OFFLINE"
-	else
+	elseif SuperchargerStatusValue.Value == "OFFLINE" then
 		wait(0.5)
 		repeat
 			SuperchargerTotalRPMValue.Value = SuperchargerTotalRPMValue.Value - 263
 			wait(0.07)
 		until
-		SuperchargerTotalRPMValue.Value == 0 or SuperchargerStatusValue.Value == "ONLINE"
+		SuperchargerTotalRPMValue.Value <= 0 or SuperchargerStatusValue.Value == "ONLINE"
 	end
 end)
 
 SuperchargerTotalRPMValue.Changed:Connect(function()
-	SuperchargerTotalRPMLable.Text = SuperchargerTotalRPMValue.Value
-	if SuperchargerTotalRPMValue.Value > 7000 then
-		SuperchargerTotalRPMLable.TextColor3 = Color3.new(1, 0.666667, 0)
-	elseif SuperchargerTotalRPMValue.Value > 15000 then
-		SuperchargerTotalRPMLable.TextColor3 = Color3.new(1, 0, 0)
+	SuperchargerTotalRPMLabel.Text = SuperchargerTotalRPMValue.Value
+	if SuperchargerTotalRPMValue.Value > 15000 then
+		SuperchargerTotalRPMLabel.TextColor3 = Color3.new(1, 0, 0)
+	elseif SuperchargerTotalRPMValue.Value < 15000 and SuperchargerTotalRPMValue.Value > 7000 then
+		SuperchargerTotalRPMLabel.TextColor3 = Color3.new(1, 0.666667, 0)
+	end
+	if SuperchargerTotalRPMValue.Value < 0 then
+		SuperchargerStatusValue.Value = 0
 	end
 end)
