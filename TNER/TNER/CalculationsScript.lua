449a1b95442620e63852134fe835d9c698869655
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-17 22:48:08 +1000

    Update CalculationsScript.lua

--- TNER/TNER/CalculationsScript.lua
+++ TNER/TNER/CalculationsScript.lua
@@ -4,36 +4,58 @@ local TNERStatusValue = Values.TNERStatusValue
 local RPMValue = Values.RPMValue
 local InputEnergyValue = Values.InputEnergyValue
 local OutputEnergyValue = Values.OutputEnergyValue
+local TNEROverloadStartUpTime = Values.TNEROverloadStartUpTime
+local TNEROverloadDelayTime = Values.TNEROverloadDelayTime
 
 local LeverPositionValue = workspace.TNERPullLever.CPU.Values.LeverPositionValue
 local PreviousLeverPositionValue = workspace.TNERPullLever.CPU.Values.PreviousLeverPositionValue
 --
 
+-- Sounds
+local OverloadLoopSound = script.Parent.Parent.Parent.SoundEmitter.OverloadLoopSound
+--
+
 -- Logic
 local RPMMultiplier = 18740
 local OutputEnergyMultiplier = 84000
+local Multiplier1 = 1
+local Multiplier2 = 1
 --
 
 -- Functions
 function Calc(Class)
 	if Class == "RPM" then
-		repeat
-			if LeverPositionValue.Value > PreviousLeverPositionValue.Value then
-				RPMValue.Value = RPMValue.Value + 3748
-			elseif LeverPositionValue.Value < PreviousLeverPositionValue.Value then
-				RPMValue.Value = RPMValue.Value - 3748
-			end
-			wait(0.03)
-		until RPMValue.Value == LeverPositionValue.Value * RPMMultiplier or TNERStatusValue.Value == "SHUT DOWN"
+		if TNERStatusValue.Value == "OVERLOAD" then
+			repeat
+				RPMValue.Value = RPMValue.Value + 9370
+				wait(1)
+			until OverloadLoopSound.IsPlaying == true or TNERStatusValue.Value == "SHUT DOWN"
+		else
+			repeat
+				if LeverPositionValue.Value > PreviousLeverPositionValue.Value then
+					RPMValue.Value = RPMValue.Value + 3748
+				elseif LeverPositionValue.Value < PreviousLeverPositionValue.Value then
+					RPMValue.Value = RPMValue.Value - (3748 * Multiplier1)
+				end
+				wait(0.03)
+			until RPMValue.Value == LeverPositionValue.Value * RPMMultiplier or TNERStatusValue.Value == "SHUT DOWN"
+		end
 	elseif Class == "OutputEnergy" then
-		repeat
-			if LeverPositionValue.Value > PreviousLeverPositionValue.Value then
+		if TNERStatusValue.Value == "OVERLOAD" then
+			repeat
 				OutputEnergyValue.Value = OutputEnergyValue.Value + 16800
-			elseif LeverPositionValue.Value < PreviousLeverPositionValue.Value then
-				OutputEnergyValue.Value = OutputEnergyValue.Value - 16800
-			end
-			wait(0.03)
-		until OutputEnergyValue.Value == LeverPositionValue.Value * OutputEnergyMultiplier or TNERStatusValue.Value == "SHUT DOWN"
+				wait(1)
+			until OverloadLoopSound.IsPlaying == true or TNERStatusValue.Value == "SHUT DOWN"
+		else
+			repeat
+				if LeverPositionValue.Value > PreviousLeverPositionValue.Value then
+					OutputEnergyValue.Value = OutputEnergyValue.Value + 16800
+				elseif LeverPositionValue.Value < PreviousLeverPositionValue.Value then
+					OutputEnergyValue.Value = OutputEnergyValue.Value - (16800 * Multiplier2)
+				end
+				wait(0.03)
+			until OutputEnergyValue.Value == LeverPositionValue.Value * OutputEnergyMultiplier or TNERStatusValue.Value == "SHUT DOWN"
+		end
 	end
 end
 --
@@ -94,4 +116,22 @@ LeverPositionValue.Changed:Connect(function()
 end)
 LeverPositionValue.Changed:Connect(function()
 	Calc("OutputEnergy")
+end)
+
+TNERStatusValue.Changed:Connect(function()
+	if TNERStatusValue.Value == "OVERLOAD" then
+		Multiplier1 = 100
+		Multiplier2 = 10
+		wait(TNEROverloadDelayTime.Value + TNEROverloadStartUpTime.Value)
+		Calc("RPM")
+	elseif TNERStatusValue.Value == "OFFLINE" then
+		Multiplier1 = 1
+		Multiplier2 = 1
+	end
+end)
+TNERStatusValue.Changed:Connect(function()
+	if TNERStatusValue.Value == "OVERLOAD" then
+		wait(TNEROverloadDelayTime.Value + TNEROverloadStartUpTime.Value)
+		Calc("OutputEnergy")
+	end
 end)
