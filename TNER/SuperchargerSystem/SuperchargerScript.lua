98b9bfd7febfb5c758ea291fad1c4d4cf29bb145
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-19 17:09:27 +1000

    Update SuperchargerScript.lua

--- TNER/SuperchargerSystem/SuperchargerScript.lua
+++ TNER/SuperchargerSystem/SuperchargerScript.lua
@@ -15,47 +15,44 @@ local DeathTriggerScript = script.Parent.Parent.Parent.DeathTriggers.DeathTrigge
 local SuperchargerIncreaseSound = workspace.TNER.SoundEmitter.SuperchargerIncreaseSound
 local SuperchargerWorkSound = workspace.TNER.SoundEmitter.SuperchargerWorkSound
 local SuperchargerDecreaseSound = workspace.TNER.SoundEmitter.SuperchargerDecreaseSound
+local OverloadStopUnsuccessSound = workspace.TNER.SoundEmitter.OverloadStopUnsuccessSound
 --
 
 -- Functions
-function DoSupercharger(Mode)
-	if Mode == "ON" then
-		if SuperchargerStatusValue.Value == "OFFLINE" then
-			SuperchargerStatusValue.Value = "INCREASE"
-			SuperchargerIncreaseSound:Play()
-			DeathTriggerScript.Enabled = true
-			for Count = 1, 6, 1 do
-				if Count == 2 or Count == 5 then
-					Flywheels["Flywheel"..Count].MainFrame.HingeConstraint.AngularVelocity = -20
-				else
-					Flywheels["Flywheel"..Count].MainFrame.HingeConstraint.AngularVelocity = 20
-				end
-				FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.Text = ("ONLINE")
-				FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.TextColor3 = Color3.new(0, 1, 0)
+function DoSupercharger()
+	if SuperchargerStatusValue.Value == "OFFLINE" and LeverValue.Value == "FULL" then
+		SuperchargerStatusValue.Value = "INCREASE"
+		if OverloadStopUnsuccessSound.IsPlaying == false then SuperchargerIncreaseSound:Play() end
+		DeathTriggerScript.Enabled = true
+		for Count = 1, 6, 1 do
+			if Count == 2 or Count == 5 then
+				Flywheels["Flywheel"..Count].MainFrame.HingeConstraint.AngularVelocity = -20
+			else
+				Flywheels["Flywheel"..Count].MainFrame.HingeConstraint.AngularVelocity = 20
 			end
-			SuperchargerSystemConsoleMonitor.SuperchargerOfflineDecal.Transparency = 1
-			SuperchargerSystemConsoleMonitor.SuperchargerOnlineDecal.Transparency = 0
-			wait(5)
-			SuperchargerStatusValue.Value = "ONLINE"
+			FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.Text = ("ONLINE")
+			FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.TextColor3 = Color3.new(0, 1, 0)
 		end
-	elseif Mode == "OFF" then
-		if SuperchargerStatusValue.Value == "ONLINE" then
-			SuperchargerStatusValue.Value = "OFFLINE"
-			for Count = 1, 6, 1 do
-				Flywheels["Flywheel"..Count].MainFrame.HingeConstraint.AngularVelocity = 0
-				FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.Text = ("OFFLINE")
-				FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.TextColor3 = Color3.new(1, 0, 0)
-			end
-			SuperchargerSystemConsoleMonitor.SuperchargerOfflineDecal.Transparency = 0
-			SuperchargerSystemConsoleMonitor.SuperchargerOnlineDecal.Transparency = 1
-			wait(2)
-			SuperchargerDecreaseSound:Play()
-			wait(0.5)
-			SuperchargerIncreaseSound:Stop()
-			SuperchargerWorkSound:Stop()
-			wait(10)
-			DeathTriggerScript.Enabled = false
+		SuperchargerSystemConsoleMonitor.SuperchargerOfflineDecal.Transparency = 1
+		SuperchargerSystemConsoleMonitor.SuperchargerOnlineDecal.Transparency = 0
+		wait(5)
+		SuperchargerStatusValue.Value = "ONLINE"
+	elseif SuperchargerStatusValue.Value == "ONLINE" and LeverValue.Value == "LOW" then
+		SuperchargerStatusValue.Value = "OFFLINE"
+		for Count = 1, 6, 1 do
+			Flywheels["Flywheel"..Count].MainFrame.HingeConstraint.AngularVelocity = 0
+			FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.Text = ("OFFLINE")
+			FlywheelsStats["Flywheel"..Count.."Status"].SurfaceGui.TextLabel.TextColor3 = Color3.new(1, 0, 0)
 		end
+		SuperchargerSystemConsoleMonitor.SuperchargerOfflineDecal.Transparency = 0
+		SuperchargerSystemConsoleMonitor.SuperchargerOnlineDecal.Transparency = 1
+		wait(2)
+		SuperchargerDecreaseSound:Play()
+		wait(0.5)
+		SuperchargerIncreaseSound:Stop()
+		SuperchargerWorkSound:Stop()
+		wait(10)
+		DeathTriggerScript.Enabled = false
 	end
 end
 --
@@ -63,28 +60,19 @@ end
 SuperchargerStatusValue.Changed:Connect(function()
 	if SuperchargerStatusValue.Value == "ONLINE" then
 		wait(SuperchargerIncreaseSound.TimeLength - 6)
-		if SuperchargerStatusValue.Value == "ONLINE" and TNERStatusValue.Value == "ONLINE" then
+		if SuperchargerStatusValue.Value == "ONLINE" and TNERStatusValue.Value == "ONLINE" and OverloadStopUnsuccessSound.IsPlaying == false then
 			SuperchargerWorkSound:Play()	
 		end
 	end
 end)
 
 LeverValue.Changed:Connect(function()
-	if LeverValue.Value == "FULL" then
-		DoSupercharger("ON")
-	else
-		DoSupercharger("OFF")
-	end
+	DoSupercharger()
 end)
 
 SuperchargerStatusValue.Changed:Connect(function()
 	wait(1)
-	if SuperchargerStatusValue.Value == "ONLINE" and LeverValue.Value == "LOW" then
-		DoSupercharger("OFF")
-	end
-	if SuperchargerStatusValue.Value == "OFFLINE" and LeverValue.Value == "FULL" then
-		DoSupercharger("ON")
-	end
+	DoSupercharger()
 end)
 
 TNERStatusValue.Changed:Connect(function()
