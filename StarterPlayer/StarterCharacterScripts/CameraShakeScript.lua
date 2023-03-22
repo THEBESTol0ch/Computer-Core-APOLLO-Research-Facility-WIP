9476a98d67191d19db316d8570bda8d30c198b52
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-22 18:50:42 +1000

    Create CameraShakeScript.lua

---
+++ StarterPlayer/StarterCharacterScripts/CameraShakeScript.lua
@@ -0,0 +1,90 @@
+-- Control
+local TweenService = game:GetService("TweenService")
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local Character = script.Parent
+local Humanoid = Character:WaitForChild("Humanoid")
+--
+
+local CameraShaker = require(game.ReplicatedStorage.CameraShaker)
+local Camera = game.Workspace.CurrentCamera
+
+local TNERShakeInTrigger = workspace.Triggers.TNERTriggers.TNERShakeInTrigger
+local TNERShakeOutTrigger = workspace.Triggers.TNERTriggers.TNERShakeOutTrigger
+local OverloadStartSound = workspace.TNER.SoundEmitter.OverloadStartSound
+local OverloadLoopSound = workspace.TNER.SoundEmitter.OverloadLoopSound
+local OverloadStopSound = workspace.TNER.SoundEmitter.OverloadStopSound
+local TNEROverloadStartUpTime = workspace.TNER.CPU.Values.TNEROverloadStartUpTime
+
+local CamShake = CameraShaker.new(Enum.RenderPriority.Camera.Value, function(shakeCf)
+	Camera.CFrame = Camera.CFrame * shakeCf
+end)
+
+game.ReplicatedStorage.CameraShakeEvent.OnClientEvent:Connect(function()
+	CamShake:Shake(CameraShaker.Presets.Earthquake)
+end)
+
+local ForceIncreaseAnimationSettings = TweenInfo.new(
+	65.2,
+	Enum.EasingStyle.Sine,
+	Enum.EasingDirection.In,
+	0,
+	false,
+	0
+)
+local ForceDecreaseAnimationSettings = TweenInfo.new(
+	9,
+	Enum.EasingStyle.Sine,
+	Enum.EasingDirection.Out,
+	0,
+	false,
+	0
+)
+
+-- Logic
+local ShakeForceValue = game.ReplicatedStorage.GameValues.ShakeForceValue
+local PlayerInShakeZone = false
+--
+
+-- Functions
+function ShakeCamera(Force)
+	local XOffset = math.random(-Force, Force) / 1000
+	local YOffset = math.random(-Force, Force) / 1000
+	local ZOffset = math.random(-Force, Force) / 1000
+	Humanoid.CameraOffset = Vector3.new(XOffset, YOffset, ZOffset)
+	wait(0.02)
+	Humanoid.CameraOffset = Vector3.new(0, 0, 0)
+end
+--
+
+OverloadStartSound.Changed:Connect(function()
+	if OverloadStartSound.IsPlaying == true then
+		wait(TNEROverloadStartUpTime.Value)
+		TweenService:Create(ShakeForceValue, ForceIncreaseAnimationSettings, { Value = 100 }):Play()
+	end
+end)
+
+OverloadStopSound.Changed:Connect(function()
+	if OverloadStopSound.IsPlaying == true then
+		wait(TNEROverloadStartUpTime.Value)
+		TweenService:Create(ShakeForceValue, ForceDecreaseAnimationSettings, { Value = 0 }):Play()
+	end
+end)
+
+TNERShakeInTrigger.Touched:Connect(function(Hit)
+	local PlayerCheck = Players:GetPlayerFromCharacter(Hit.Parent)
+	if PlayerCheck and PlayerCheck == Player then
+		PlayerInShakeZone = true
+		if OverloadStartSound.IsPlaying == true or OverloadLoopSound.IsPlaying == true or OverloadStopSound.IsPlaying == true then
+			repeat
+				ShakeCamera(ShakeForceValue.Value)
+			until PlayerInShakeZone == false
+		end
+	end
+end)
+TNERShakeOutTrigger.Touched:Connect(function(Hit)
+	local PlayerCheck = Players:GetPlayerFromCharacter(Hit.Parent)
+	if PlayerCheck and PlayerCheck == Player then
+		PlayerInShakeZone = false
+	end
+end)
