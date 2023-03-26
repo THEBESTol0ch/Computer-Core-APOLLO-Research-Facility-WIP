30435b3dea33539e9e41aa4c8adc6c7e9061c83e
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-26 21:51:53 +1000

    Update DustyHUDLocalScript.lua

--- HUD/DustyHUD/DustyHUDLocalScript.lua
+++ HUD/DustyHUD/DustyHUDLocalScript.lua
@@ -1,14 +1,12 @@
 local DustyHUD = script.Parent.DustyHUD
-local FacilityDestructionEffectStatusValue = workspace.FacilityDestructionEffect.CPU.Values.FacilityDestructionEffectStatusValue
-local BlackoutStatusValue = workspace.FacilityLight.CPU.Values.BlackoutStatusValue
+local FacilityDestructionEffectValue = workspace.FacilityDestructionEffect.CPU.Values.FacilityDestructionEffectValue
 
-FacilityDestructionEffectStatusValue.Changed:Connect(function()
-	if FacilityDestructionEffectStatusValue.Value == 1 and BlackoutStatusValue.Value == 0 then
+FacilityDestructionEffectValue.Changed:Connect(function()
+	if FacilityDestructionEffectValue.Value == "SHAKING" then
 		DustyHUD.Visible = true
 		DustyHUD:TweenPosition(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 5, false)
 		DustyHUD:TweenSize(UDim2.new(1, 0, 1, 0), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 5, false)
-	end
-	if FacilityDestructionEffectStatusValue.Value == 0 then
+	elseif FacilityDestructionEffectValue.Value == "SHAKED" then
 		wait(4)
 		DustyHUD:TweenPosition(UDim2.new(-7, 0, -7, 0), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 5, false)
 		DustyHUD:TweenSize(UDim2.new(15, 0, 15, 0), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 5, false)
