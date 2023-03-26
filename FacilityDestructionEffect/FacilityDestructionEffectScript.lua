ccc3b7e1718cb5420b726c8348f7cc8fc7d6694a
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-03-26 21:51:34 +1000

    Update FacilityDestructionEffectScript.lua

--- FacilityDestructionEffect/FacilityDestructionEffectScript.lua
+++ FacilityDestructionEffect/FacilityDestructionEffectScript.lua
@@ -3,33 +3,43 @@ local FacilityDestructionEffectValue = script.Parent.Parent.Values.FacilityDestr
 --
 
 -- Items
-local DustEmitters = script.Parent.Parent.Parent.DustEmitters:GetChildren()
-local SmokeEmitters = script.Parent.Parent.Parent.SmokeEmitters:GetChildren()
+local DustEmitters = script.Parent.Parent.Parent.DustEmitters
+local SmokeEmitters = script.Parent.Parent.Parent.SmokeEmitters
+--
+
+-- Sounds
+local FacilityDestructionSounds = game.SoundService.FacilityDestructionSounds
 --
 
 -- Functions
-function DoEffect()
-	FacilityDestructionEffectValue.Value = "SHAKING"
-	wait(2)
-	for _, Object in pairs(DustEmitters) do
-		Object.ParticleEmitter.Enabled = true
+function DoEffect(Mode)
+	if Mode == "ON" then
+		for _, Object in pairs(DustEmitters:GetChildren()) do
+			Object.ParticleEmitter.Enabled = true
+		end
+		for _, Object in pairs(SmokeEmitters:GetChildren()) do
+			Object.Smoke.Enabled = true
+		end
+	elseif Mode == "OFF" then
+		for _, Object in pairs(DustEmitters:GetChildren()) do
+			Object.ParticleEmitter.Enabled = false
+		end
+		for _, Object in pairs(SmokeEmitters:GetChildren()) do
+			Object.Smoke.Enabled = false
+		end
 	end
-	for _, Object in pairs(SmokeEmitters) do
-		Object.Smoke.Enabled = true
-	end
-	wait(10)
-	for _, Object in pairs(DustEmitters) do
-		Object.ParticleEmitter.Enabled = false
-	end
-	for _, Object in pairs(SmokeEmitters) do
-		Object.Smoke.Enabled = false
-	end
-	FacilityDestructionEffectValue.Value = "SHAKED"
+	
 end
 --
 
 FacilityDestructionEffectValue.Changed:Connect(function()
 	if FacilityDestructionEffectValue.Value == "SHAKE" then
-		DoEffect()
+		FacilityDestructionEffectValue.Value = "SHAKING"
+		FacilityDestructionSounds["FacilityDestructionSound"..math.random(1, 7)]:Play()
+		wait(2)
+		DoEffect("ON")
+		wait(10)
+		DoEffect("OFF")
+		FacilityDestructionEffectValue.Value = "SHAKED"
 	end
 end)
