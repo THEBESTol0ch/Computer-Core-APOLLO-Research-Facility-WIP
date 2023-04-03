62d16a4a87ab41ba69aa553908351c0622245d4f
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-03 19:10:42 +1000

    Create DoorCloseButtonScript.lua

---
+++ OfficeElevator/DoorCloseButtonScript.lua
@@ -0,0 +1,23 @@
+-- Control
+local Button = script.Parent.Button
+--
+
+-- Values
+local ElevatorStatusValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorStatusValue
+local ElevatorDoorControlValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorDoorControlValue
+--
+
+-- Sounds
+local ButtonSound = script.Parent.SoundEmitter.ButtonSound
+--
+
+Button.ClickDetector.MouseClick:Connect(function()
+	Button.ClickDetector.MaxActivationDistance = 0
+	ButtonSound:Play()
+	wait(0.3)
+	if ElevatorStatusValue.Value == "OPENED" then
+		ElevatorDoorControlValue.Value = "CLOSE"
+	end
+	wait(0.7)
+	Button.ClickDetector.MaxActivationDistance = 10
+end)
