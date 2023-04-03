ef67f5b80c4b7807e481bface8f6e96dc4947912
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-03 19:09:47 +1000

    Update CabinButtonScript.lua

--- OfficeElevator/CabinButtonScript.lua
+++ OfficeElevator/CabinButtonScript.lua
@@ -6,6 +6,7 @@ local Button = script.Parent.Button
 local ElevatorCurrentFloorValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorCurrentFloorValue
 local ElevatorTargetFloorValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorTargetFloorValue
 local ElevatorStatusValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorStatusValue
+local ElevatorDoorControlValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorDoorControlValue
 --
 
 -- Sounds
@@ -20,8 +21,10 @@ Button.ClickDetector.MouseClick:Connect(function()
 	Button.ClickDetector.MaxActivationDistance = 0
 	ButtonSound:Play()
 	wait(0.3)
-	if ElevatorStatusValue.Value == "CLOSED" or ElevatorStatusValue.Value == "OPENED" then
-		ElevatorTargetFloorValue.Value = FloorNumber
+	if ElevatorCurrentFloorValue.Value == tonumber(FloorNumber) and ElevatorStatusValue.Value == "CLOSED" then
+		ElevatorDoorControlValue.Value = "OPEN"
+	elseif ElevatorStatusValue.Value == "CLOSED" or ElevatorStatusValue.Value == "OPENED" then
+		ElevatorTargetFloorValue.Value = tonumber(FloorNumber)
 		if ElevatorCurrentFloorValue.Value ~= ElevatorTargetFloorValue.Value then Button.Material = "Neon" end
 	end
 	wait(0.7)
