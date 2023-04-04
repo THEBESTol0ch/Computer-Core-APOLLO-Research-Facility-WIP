b7e597424599acad05049ff3640b51e330bcf21c
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-04 19:19:00 +1000

    Update FacilityDestructionEffectScript.lua

--- FacilityDestructionEffect/FacilityDestructionEffectScript.lua
+++ FacilityDestructionEffect/FacilityDestructionEffectScript.lua
@@ -5,6 +5,7 @@ local FacilityDestructionEffectValue = script.Parent.Parent.Values.FacilityDestr
 -- Items
 local DustEmitters = script.Parent.Parent.Parent.DustEmitters
 local SmokeEmitters = script.Parent.Parent.Parent.SmokeEmitters
+local Cars = script.Parent.Parent.Parent.Cars
 --
 
 -- Sounds
@@ -28,7 +29,21 @@ function DoEffect(Mode)
 			Object.Smoke.Enabled = false
 		end
 	end
-	
+end
+function TiggerCar()
+	local Car = "Car"..math.random(1, 8)
+	local CarAlarmSound = "CarAlarm"..math.random(1, 2).."Sound"
+	Cars[Car].EarthquakeEventSet.Lights[CarAlarmSound]:Play()
+	repeat
+		Cars[Car].EarthquakeEventSet.Lights.Transparency = 0
+		Cars[Car].EarthquakeEventSet.FrontArtificalLight.SpotLight.Enabled = true
+		Cars[Car].EarthquakeEventSet.BackArtificalLight.SpotLight.Enabled = true
+		wait(1)
+		Cars[Car].EarthquakeEventSet.Lights.Transparency = 1
+		Cars[Car].EarthquakeEventSet.FrontArtificalLight.SpotLight.Enabled = false
+		Cars[Car].EarthquakeEventSet.BackArtificalLight.SpotLight.Enabled = false
+		wait(1)
+	until Cars[Car].EarthquakeEventSet.Lights[CarAlarmSound].IsPlaying == false
 end
 --
 
@@ -43,3 +58,36 @@ FacilityDestructionEffectValue.Changed:Connect(function()
 		FacilityDestructionEffectValue.Value = "SHAKED"
 	end
 end)
+
+FacilityDestructionEffectValue.Changed:Connect(function()
+	if FacilityDestructionEffectValue.Value == "SHAKING" then
+		wait(2)
+		TiggerCar()
+	end
+end)
+
+FacilityDestructionEffectValue.Changed:Connect(function()
+	if FacilityDestructionEffectValue.Value == "SHAKING" then
+		wait(5.5)
+		TiggerCar()
+	end
+end)
+
+FacilityDestructionEffectValue.Changed:Connect(function()
+	if FacilityDestructionEffectValue.Value == "SHAKING" then
+		wait(3.2)
+		TiggerCar()
+	end
+end)
+
+--workspace.PumpTestButton.ClickDetector.MouseClick:Connect(function()
+--	FacilityDestructionEffectValue.Value = "SHAKE"
+--end)
+
+--workspace.AlphaCoreTestButton.ClickDetector.MouseClick:Connect(function()
+--	FacilityDestructionEffectValue.Value = "SHAKE"
+--end)
+
+--workspace.CRCExplosionTestButton.ClickDetector.MouseClick:Connect(function()
+--	FacilityDestructionEffectValue.Value = "SHAKE"
+--end)
