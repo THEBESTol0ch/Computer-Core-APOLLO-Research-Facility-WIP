8479439380d29c688d0c60aa5ff716f79d6ecdd5
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-11 18:20:51 +1000

    Create WeldTriggerScript.lua

---
+++ MaintenanceElevator/WeldTriggerScript.lua
@@ -0,0 +1,21 @@
+-- Values
+local ElevatorStatusValue = script.Parent.Parent.Parent.Parent.CPU.Values.ElevatorStatusValue
+--
+
+-- Items
+local WeldTrigger = script.Parent
+--
+
+WeldTrigger.Touched:Connect(function(Hit)
+	if Hit.Parent:FindFirstChild("Humanoid") then
+		local HumanoidRootPart = Hit.Parent.HumanoidRootPart
+		local Weld = Instance.new("WeldConstraint")
+		Weld.Part0 = HumanoidRootPart
+		Weld.Part1 = WeldTrigger
+		Weld.Parent = script.Parent
+		repeat
+			wait(0.1)
+		until ElevatorStatusValue.Value == "ARRIVED"
+		Weld:Destroy()
+	end
+end)
