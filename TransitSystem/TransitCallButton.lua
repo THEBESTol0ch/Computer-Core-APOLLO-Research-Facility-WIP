20791acfd363fa932b6e9130803b0ed9168b26ef
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-18 13:05:51 +1000

    Create TransitCallButton.lua

---
+++ TransitSystem/TransitCallButton.lua
@@ -0,0 +1,47 @@
+-- Control
+local Button = script.Parent.Button
+--
+
+-- Values
+local TransitCurrentStationValue = script.Parent.Parent.Parent.Parent.Parent.CPU.Values.TransitCurrentStationValue
+local TransitTargetStationValue = script.Parent.Parent.Parent.Parent.Parent.CPU.Values.TransitTargetStationValue
+local TransitStatusValue = script.Parent.Parent.Parent.Parent.Parent.CPU.Values.TransitStatusValue
+--
+
+-- Sounds
+local TransitCallButtonSound = script.Parent.Button.TransitCallButtonSound
+--
+
+-- Logic
+local StationName = script.Parent.Parent.Name:split("_")[1]
+--
+
+Button.ClickDetector.MouseClick:Connect(function()
+	Button.ClickDetector.MaxActivationDistance = 0
+	TransitCallButtonSound:Play()
+	wait(1.3)
+	if TransitStatusValue.Value == "OPENED" then
+		TransitTargetStationValue.Value = StationName
+		if not (TransitCurrentStationValue.Value == TransitTargetStationValue.Value) then Button.BrickColor = BrickColor.new("Neon orange") end
+	end
+	wait(1)
+	Button.ClickDetector.MaxActivationDistance = 10
+end)
+
+TransitStatusValue.Changed:Connect(function()
+	if TransitStatusValue.Value == "OPENED" then
+		if TransitCurrentStationValue.Value == StationName then
+			Button.BrickColor = BrickColor.new("Forest green")
+			Button.Material = ("Plastic")
+		else
+			Button.BrickColor = BrickColor.new("Forest green")
+			Button.Material = ("Neon")
+		end
+	elseif TransitStatusValue.Value == "EMERGENCY STOP" then
+		Button.BrickColor = BrickColor.new("Forest green")
+		Button.Material = ("Plastic")
+	elseif TransitStatusValue.Value == "PARKING MODE" then
+		Button.BrickColor = BrickColor.new("Forest green")
+		Button.Material = ("Plastic")
+	end
+end)
