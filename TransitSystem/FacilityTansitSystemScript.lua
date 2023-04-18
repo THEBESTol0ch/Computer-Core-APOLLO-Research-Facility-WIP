fdaf3297d6b9bc807c6d01b293fbaf0e7728463e
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-18 13:04:06 +1000

    Create FacilityTansitSystemScript.lua

---
+++ TransitSystem/FacilityTansitSystemScript.lua
@@ -0,0 +1,61 @@
+-- Values
+local FacilityTransitSystemControlValue = script.Parent.Parent.Values.FacilityTransitSystemControlValue
+local FacilityTransitSystemStatusValue = script.Parent.Parent.Values.FacilityTransitSystemStatusValue
+local TransitStatusValue1 = script.Parent.Parent.Parent.Transit1.CPU.Values.TransitStatusValue
+--
+
+-- Sounds
+local TransitStationAmbientSound = game.SoundService.Ambients.TransitStationAmbientSound
+local TransitStationPowerOffAmbientSound = game.SoundService.Ambients.TransitStationPowerOffAmbientSound
+local TransitSystemPowerDownAmbientSound = game.SoundService.Ambients.TransitSystemPowerDownAmbientSound
+local TransitSystemPowerUpAmbientSound = game.SoundService.Ambients.TransitSystemPowerUpAmbientSound
+
+local LocalTransitSystemPowerDownSound = workspace.FacilityTransitSystem.TransitSystemSoundEmitter.TransitSystemPowerDownSound
+local LocalTransitSystemPowerUpSound = workspace.FacilityTransitSystem.TransitSystemSoundEmitter.TransitSystemPowerUpSound
+--
+
+-- Logic
+local CanEvent1 = true
+local CanEvent2 = false
+--
+
+-- Functions
+function DoCheck()
+	if FacilityTransitSystemControlValue.Value == "DISABLE" and FacilityTransitSystemStatusValue.Value == "ONLINE" then
+		FacilityTransitSystemStatusValue.Value = "SHUT DOWN"
+	elseif FacilityTransitSystemControlValue.Value == "ENABLE" and FacilityTransitSystemStatusValue.Value == "OFFLINE" then
+		FacilityTransitSystemStatusValue.Value = "POWER ON"
+	end
+	if FacilityTransitSystemStatusValue.Value == "SHUT DOWN" and CanEvent1 == true then
+		if TransitStatusValue1.Value == "PARKING MODE" then
+			CanEvent1 = false
+			TransitStationAmbientSound:Stop()
+			TransitStationPowerOffAmbientSound:Play()
+			TransitSystemPowerDownAmbientSound:Play()
+			LocalTransitSystemPowerDownSound:Play()
+			wait(30)
+			CanEvent2 = true
+			FacilityTransitSystemStatusValue.Value = "OFFLINE"
+		end
+	elseif FacilityTransitSystemStatusValue.Value == "POWER ON" and CanEvent2 == true then
+		CanEvent2 = false
+		TransitStationAmbientSound:Play()
+		TransitStationPowerOffAmbientSound:Stop()
+		TransitSystemPowerUpAmbientSound:Play()
+		LocalTransitSystemPowerUpSound:Play()
+		wait(80)
+		CanEvent1 = true
+		FacilityTransitSystemStatusValue.Value = "ONLINE"
+	end
+end
+--
+
+FacilityTransitSystemControlValue.Changed:Connect(function()
+	DoCheck()
+end)
+FacilityTransitSystemStatusValue.Changed:Connect(function()
+	DoCheck()
+end)
+TransitStatusValue1.Changed:Connect(function()
+	DoCheck()
+end)
