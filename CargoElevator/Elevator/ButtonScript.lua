82b9d854d86e948fdfb83f4dc8ef533786fe67a1
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-21 02:16:54 +1000

    Create ButtonScript.lua

---
+++ CargoElevator/Elevator/ButtonScript.lua
@@ -0,0 +1,33 @@
+-- Control
+local Button = script.Parent
+--
+
+-- Values
+local ElevatorCurrentFloorValue = script.Parent.Parent.Parent.Parent.Parent.CPU.Values.ElevatorCurrentStationValue
+local ElevatorTargetFloorValue = script.Parent.Parent.Parent.Parent.Parent.CPU.Values.ElevatorTargetStationValue
+local ElevatorStatusValue = script.Parent.Parent.Parent.Parent.Parent.CPU.Values.ElevatorStatusValue
+--
+
+-- Sounds
+local ElevatorCallButtonSound = Button.TransitCallButtonSound
+--
+
+-- Logic
+local StationNumber = script.Parent.Parent.Name:split("_")[2]
+--
+
+Button.ClickDetector.MouseClick:Connect(function()
+	Button.ClickDetector.MaxActivationDistance = 0
+	ElevatorCallButtonSound:Play()
+	wait(1)
+	if ElevatorStatusValue.Value == "OPENED" then
+		ElevatorTargetFloorValue.Value = tonumber(StationNumber)
+		if ElevatorCurrentFloorValue.Value ~= ElevatorTargetFloorValue.Value then Button.Material = "Neon" end
+	end
+	wait(1.3)
+	Button.ClickDetector.MaxActivationDistance = 10
+end)
+
+ElevatorCurrentFloorValue.Changed:Connect(function()
+	if ElevatorCurrentFloorValue.Value == ElevatorTargetFloorValue.Value then Button.Material = "Plastic" end
+end)
