fb321fa8c2156c42fd8645a5db069f3f59559933
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-26 17:58:42 +1000

    Update FacilityAlarmsScript.lua

--- ServerScriptService/FacilityAlarmsScript.lua
+++ ServerScriptService/FacilityAlarmsScript.lua
@@ -1,5 +1,10 @@
 -- Values
 local FacilitySystemsHackDevicesStatusValue = workspace.FacilitySystemsHackDevicesSystem.Values.FacilitySystemsHackDevicesStatusValue
+
+local KeyTurnedValue1 = workspace.CRCConsole.KeyLock1.CPU.Values.KeyTurnedValue
+local KeyTurnedValue2 = workspace.CRCConsole.KeyLock2.CPU.Values.KeyTurnedValue
+
+local CRCIntegrityValue = workspace.CentralReactorCore.CentralCore.CPU.Values.IntegrityValue
 --
 
 -- Sounds
@@ -9,6 +14,23 @@ local FacilityAlarmAmbientSound3 = game.SoundService.Ambients.FacilityAlarmAmbie
 local BMAirRaidAmbientSound = game.SoundService.Ambients.BMAirRaidAmbientSound
 --
 
+-- Functions
+function DoCheck()
+	if KeyTurnedValue1.Value == true and KeyTurnedValue2.Value == true then
+		FacilityAlarmAmbientSound1:Play()
+	else
+		FacilityAlarmAmbientSound1:Stop()
+	end
+end
+--
+
+KeyTurnedValue1.Changed:Connect(function()
+	DoCheck()
+end)
+KeyTurnedValue2.Changed:Connect(function()
+	DoCheck()
+end)
+
 FacilitySystemsHackDevicesStatusValue.Changed:Connect(function()
 	if FacilitySystemsHackDevicesStatusValue.Value == "HACK" then
 		wait(2)
@@ -16,5 +38,16 @@ FacilitySystemsHackDevicesStatusValue.Changed:Connect(function()
 	elseif FacilitySystemsHackDevicesStatusValue.Value == "CLEAR" then
 		wait(2)
 		FacilityAlarmAmbientSound3:Stop()
+	end
+end)
+
+CRCIntegrityValue.Changed:Connect(function()
+	if CRCIntegrityValue.Value == 20 then
+		wait(20)
+		FacilityAlarmAmbientSound3:Play()
+	elseif CRCIntegrityValue.Value == 0 then
+		wait(20)
+		FacilityAlarmAmbientSound2:Play()
+		BMAirRaidAmbientSound:Play()
 	end
 end)
