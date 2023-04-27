98230487768554001b22a2336ee40ef3b5d75d39
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-27 16:30:54 +1000

    Update FacilitySystemsHackDevicesSystemScritpt.lua

--- FacilitySystemsHackDevicesSystem/FacilitySystemsHackDevicesSystemScritpt.lua
+++ FacilitySystemsHackDevicesSystem/FacilitySystemsHackDevicesSystemScritpt.lua
@@ -2,18 +2,22 @@
 local FacilitySystemsHackDevicesStatusValue = workspace.FacilitySystemsHackDevicesSystem.Values.FacilitySystemsHackDevicesStatusValue
 
 local Fuse5HackDeviceValue = workspace.HackTriggers.Fuse5HackTrigger.MilitaryLaptop.CPU.Values.LaptopStatusValue
+local ApolloHackDeviceValue = workspace.HackTriggers.ApolloHackTrigger.MilitaryLaptop.CPU.Values.LaptopStatusValue
 --
 
 -- Functions
 function DoCheck()
-	if Fuse5HackDeviceValue.Value == "HACKING" then
+	if Fuse5HackDeviceValue.Value == "HACKING" or ApolloHackDeviceValue.Value == "HACKING" then
 		FacilitySystemsHackDevicesStatusValue.Value = "HACK"
-	elseif Fuse5HackDeviceValue.Value == "" then
+	elseif Fuse5HackDeviceValue.Value == "" and ApolloHackDeviceValue.Value == "" then
 		FacilitySystemsHackDevicesStatusValue.Value = "CLEAR"
 	end
 end
 --
 
 Fuse5HackDeviceValue.Changed:Connect(function()
+	DoCheck()
+end)
+ApolloHackDeviceValue.Changed:Connect(function()
 	DoCheck()
 end)
