6d3cf268530ba31d4f6048ea081fd0f11a1a3523
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-04-27 16:50:24 +1000

    Create AdminCommands.lua

---
+++ AdminCommands.lua
@@ -0,0 +1,80 @@
+:A, Set_CRC_Integrity_To NUMBER
+
+:A, Damage CRC
+:A, Destroy Alpha Core
+:A, Destroy Beta Core
+:A, Destroy Gamma Core
+:A, Destroy Delta Core
+
+:A, Initiate Earthquake
+
+:A, Initiate Blackout
+:A, Deactivate Blackout
+
+:A, Refuel Diesel Generators
+
+:A, Destroy Random Pump
+:A, Destroy Water Pump
+:A, Destroy Hot Water Pump
+:A, Destroy Fire Protection Water Pump
+:A, Destroy Coolant Pump
+
+:A, Set Intercom Mode To 1
+:A, Set Intercom Mode To 2
+:A, Set Intercom Mode To 3
+
+:A, Turn On Surface Alarm
+:A, Turn Off Surface Alarm
+
+:A, Turn On Facility Alarm 1
+:A, Turn Off Facility Alarm 1
+
+:A, Turn On Facility Alarm 2
+:A, Turn Off Facility Alarm 2
+
+:A, Turn On Facility Alarm 3
+:A, Turn Off Facility Alarm 3
+
+:A, Turn On Facility Emergency Lamps
+:A, Turn Off Facility Emergency Lamps
+
+:A, Close Facility Blast Doors
+:A, Open Facility Blast Doors
+
+:A, Disable Transit System
+:A, Enable Transit System
+
+:A, Initiate Full Facility Containment Lockdown
+:A, Deactivate Full Facility Containment Lockdown
+
+:A, Disable Facility Transit System
+:A, Enable Facility Transit System
+:A, Set Transit Lighting Mode To STANDARD
+:A, Set Transit Lighting Mode To RGB
+
+:A, Call Military Forces
+:A, Open Military Trucks
+:A, Call Off Military Forces
+
+:A, Give Me Ammo
+:A, Give Me M4A1
+:A, Give Me HK USP Match
+:A, Give Me SPAS-12
+
+:A, Give Me A. Keycard
+:A, Give Me E. Keycard
+:A, Give Me S. O. Keycard
+
+:A, Give Me Military Laptop
+
+:A, Give Me Blast Door Hack Tool
+
+:A, Give Me Fuel Cell
+:A, Give Me Depleted Fuel Cell
+
+:A, Give Me Gas Can
+:A, Give Me Empty Gas Can
+
+:A, Give Me Flashlight
+
+:A, Make USERNAME Chainsaw Man // [F] - Starter [E] - Shutdown [K] - Increase stats [L] - Hide All [LMB] - Standard attack [Q] - Ultimate
