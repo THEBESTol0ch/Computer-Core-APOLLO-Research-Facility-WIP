7cf00143635b84baddcab8c5922902b1d4b26f4d
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2023-07-31 11:26:12 +1000

    Create HKUSPMatch_HolsterModelScript.lua

---
+++ StarterPlayer/StarterCharacterScripts/HKUSPMatch_HolsterModelScript.lua
@@ -0,0 +1,34 @@
+local Character = script.Parent
+local Backpack = game.Players[Character.Name].Backpack
+local Item = script.Name:split("_")[1]
+local HolsterModel = game.ReplicatedStorage[Item.."Stuff"][Item.."HolsterModel"]
+
+function CreateHolsterModel()
+	if not Character:FindFirstChild(HolsterModel.Name) then
+		NewHolsterModel = HolsterModel:Clone()
+		NewHolsterModel.Parent = Character
+		NewHolsterModel.Connector.Weld.Part1 = Character.RightUpperLeg
+	end
+end
+
+Backpack.ChildAdded:Connect(function(Child)
+	if Child.Name == Item then
+		CreateHolsterModel()
+		for _, Item in pairs(NewHolsterModel:GetChildren()) do
+			if not (Item:IsA("WeldConstraint") or Item:IsA("Weld") or Item.Name == "Connector") then
+				Item.Transparency = 0
+			end
+		end
+	end
+end)
+
+Backpack.ChildRemoved:Connect(function(Child)
+	if Child.Name == Item then
+		CreateHolsterModel()
+		for _, Item in pairs(NewHolsterModel:GetChildren()) do
+			if not (Item:IsA("WeldConstraint") or Item:IsA("Weld") or Item.Name == "Connector") then
+				Item.Transparency = 1
+			end
+		end
+	end
+end)
