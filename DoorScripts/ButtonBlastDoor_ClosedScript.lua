e7f9e3059ee32a82b7e852235e99fb3305955062
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2024-01-17 15:38:38 +1000

    Create ButtonBlastDoor_ClosedScript.lua

---
+++ DoorScripts/ButtonBlastDoor_ClosedScript.lua
@@ -0,0 +1,74 @@
+-- Control
+local TweenService = game:GetService("TweenService")
+local BlastDoor = script.Parent
+local Button1 = BlastDoor.Button_1.Button
+local Button2 = BlastDoor.Button_2.Button
+--
+
+-- Items
+local Door = BlastDoor.Door.Door
+local DoorOpenPos = BlastDoor.DoorOpenPos
+local DoorClosePos = BlastDoor.DoorClosePos
+
+local EmergencyLamps = BlastDoor.EmergencyLamps
+--
+
+-- Sounds
+local OpenSound = BlastDoor.Door.SoundEmitter.BlastDoorOpenSound
+local CloseSound = BlastDoor.Door.SoundEmitter.BlastDoorCloseSound
+--
+
+-- Logic
+local CanEvent = true
+local DoorClosed = true
+
+local DoorAnimationSettings = TweenInfo.new(
+	5.9,
+	Enum.EasingStyle.Sine,
+	Enum.EasingDirection.InOut,
+	0,
+	false,
+	0
+)
+--
+
+local BlastDoorOpenAnim = TweenService:Create(Door, DoorAnimationSettings, { CFrame = DoorOpenPos.CFrame })
+local BlastDoorCloseAnim = TweenService:Create(Door, DoorAnimationSettings, { CFrame = DoorClosePos.CFrame })
+
+-- Functions
+function DoLamps(Mode, Speed)
+	for _, Lamp in pairs(EmergencyLamps:GetChildren()) do
+		Lamp.Lamp.SpotLight.Enabled = Mode
+		Lamp.Lamp.HingeConstraint.AngularVelocity = Speed
+	end
+end
+function DoDoor()
+	if DoorClosed and CanEvent then
+		CanEvent = false
+		OpenSound:Play()
+		BlastDoorOpenAnim:Play()
+		DoLamps(true, 5)
+		wait(5.9)
+		DoLamps(false, 0)
+		DoorClosed = false
+		CanEvent = true
+	else
+		CanEvent = false
+		CloseSound:Play()
+		BlastDoorCloseAnim:Play()
+		DoLamps(true, 5)
+		wait(5.9)
+		DoLamps(false, 0)
+		DoorClosed = false
+		CanEvent = true
+	end
+end
+--
+
+Button1.ClickDetector.MouseClick:Connect(function()
+	DoDoor()
+end)
+
+Button2.ClickDetector.MouseClick:Connect(function()
+	DoDoor()
+end)
