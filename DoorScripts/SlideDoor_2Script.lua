5c39ba8c0b0a86184a6f5505bb800fbc9059ce32
Ivan <105137450+THEBESTol0ch@users.noreply.github.com>
2024-01-18 11:02:53 +1000

    Create SlideDoor_2Script.lua

---
+++ DoorScripts/SlideDoor_2Script.lua
@@ -0,0 +1,43 @@
+-- Control
+local TweenService = game:GetService("TweenService")
+local Trigger = script.Parent.Trigger
+--
+
+-- Items
+local SlideDoor = script.Parent
+local SoundEmitter = SlideDoor.SoundEmitter
+
+local Door = SlideDoor.Door.Door
+local DoorClosePos = SlideDoor.DoorClosePos
+local DoorOpenPos = SlideDoor.DoorOpenPos
+--
+
+-- Sounds
+local OpenSound = SoundEmitter.SlideDoorOpenSound
+local CloseSound = SoundEmitter.SlideDoorCloseSound
+--
+
+-- Logic
+local DoorAnimationSettings = TweenInfo.new(
+	0.8,
+	Enum.EasingStyle.Quint,
+	Enum.EasingDirection.Out,
+	0,
+	false,
+	0
+)
+--
+
+local DoorOpenAnim = TweenService:Create(Door, DoorAnimationSettings, { CFrame = DoorOpenPos.CFrame })
+local DoorCloseAnim = TweenService:Create(Door, DoorAnimationSettings, { CFrame = DoorClosePos.CFrame })
+
+Trigger.Touched:Connect(function()
+	Trigger.CanTouch = false
+	DoorOpenAnim:Play()
+	OpenSound:Play()
+	wait(3)
+	DoorCloseAnim:Play()
+	CloseSound:Play()
+	wait(1)
+	Trigger.CanTouch = true
+end)
